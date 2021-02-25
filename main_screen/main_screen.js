bb4ef6adbe1cdcf549d835fe3f63bb1400452b9b
andrewfromtver <andrewsarkisyan@gmail.com>
2021-02-25 23:50:00 +0300

    -

--- main_screen/main_screen.js
+++ main_screen/main_screen.js
@@ -3,14 +3,11 @@ let userData = {}
 window.onload = () => {
     if (sessionStorage.userData) {
         userData = JSON.parse(sessionStorage.userData)
+        userInfo()
     }
     else {
         window.location = '/login_form'
     }
-}
-
-logoutRequest = () => {
-    window.location = '/login_form'
 }
 
 userInfo = () => {
@@ -40,7 +37,7 @@ userInfo = () => {
                 <input id="ocupation"></input>
             </div>
             <div class="actionstab">
-                <button id="save">Save</button>
+                <button onclick="saveUserData()" id="save">Save</button>
             </div>
         </div>
         <br>
@@ -55,11 +52,11 @@ userInfo = () => {
         </div>
         <br>
         <div class="tariff">
-            <div id="onetime">
+            <div id="onetime" onclick="changeUserTariff(false)">
                 <img src="./onetime.png" alt="onetime">
                 <p>One time payment</p>
             </div>
-            <div id="monthly">
+            <div id="monthly" onclick="changeUserTariff(true)">
                 <img src="./monthly.png" alt="monthly">
                 <p>Monthly plan</p>
             </div>
@@ -91,7 +88,7 @@ userInfo = () => {
                 <input id="social"></input>
             </div>
             <div class="actionstab">
-                <button id="save">Save</button>
+                <button onclick="saveUserContacts()" id="save">Save</button>
             </div>
         </div>
         <br>
@@ -114,6 +111,43 @@ userInfo = () => {
     document.querySelector('#email').value  = userData.email
     document.querySelector('#site').value  = userData.site
     document.querySelector('#social').value  = userData.social
+}
+
+saveUserData = () => {
+    userData.name = document.querySelector('#name').value
+    userData.surname = document.querySelector('#surname').value
+    userData.age = document.querySelector('#age').value
+    userData.ocupation = document.querySelector('#ocupation').value
+
+    sessionStorage.setItem('userData', JSON.stringify(userData))
+}
+
+changeUserTariff = (monthly = true) => {
+    if (monthly) {
+        userData.subscribed = true
+        document.querySelector('#monthly').style.boxShadow = '0 5px 25px #0589ed'
+        document.querySelector('#onetime').style.boxShadow = ''
+    }
+    else {
+        userData.subscribed = false
+        document.querySelector('#monthly').style.boxShadow = ''
+        document.querySelector('#onetime').style.boxShadow = '0 5px 25px #0589ed'
+    }
+    sessionStorage.setItem('userData', JSON.stringify(userData))
+}
+
+saveUserContacts = () => {
+    userData.phone = document.querySelector('#phone').value
+    userData.email = document.querySelector('#email').value
+    userData.site = document.querySelector('#site').value
+    userData.social = document.querySelector('#social').value
+
+    sessionStorage.setItem('userData', JSON.stringify(userData))
+}
+
+logoutRequest = () => {
+    window.location = '/login_form'
+    sessionStorage.removeItem('userData')
 }
 
 home = () => {
