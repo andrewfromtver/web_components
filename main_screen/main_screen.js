a0f6051904d4251642cb5de2c79c66e608864307
andrewfromtver <59865403+andrewfromtver@users.noreply.github.com>
2021-02-25 19:25:25 +0300

    Add files via upload

---
+++ main_screen/main_screen.js
@@ -0,0 +1,19 @@
+logoutRequest = () => {
+    window.location = '/login_form'
+}
+
+userInfo = () => {
+    window.location = '/main_screen/#user_info'
+}
+
+home = () => {
+    window.location = '/main_screen/#home'
+}
+
+activities = () => {
+    window.location = '/main_screen/#activities'
+}
+
+contacts = () => {
+    window.location = '/main_screen/#contacts'
+}
